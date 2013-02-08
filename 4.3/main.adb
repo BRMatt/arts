ede44c75083581925ae38bc3955a8969b209ac1c
Matt Button <matthew@sigswitch.com>
2013-02-08 17:35:24 +0000

    Adjustments to ant's solution

--- 4.3/main.adb
+++ 4.3/main.adb
@@ -8,68 +8,84 @@ with Ada.Calendar; use Ada.Calendar;
 --
 
 procedure Main is
-  LastBall	:	Chute.Ball_Detected;
+  LastBall  :  Chute.Ball_Detected;
   ReleasedBall : Chute.Ball_Detected;
-  flag : boolean;
+  
   suspend : boolean;
+  ReleasedAt   : Time;
+  ballsReleased : Integer := 0;
+  
   task type releaseball;
-  task body releaseball is
+  task body releaseball iswith MaRTE_OS;
+with Chute; use Chute;
+with Text_IO;
+with Ada.Calendar; use Ada.Calendar;
+--
+-- Best so far
+-- 0.45 & 1.9
+--
+
+procedure Main is
   begin
     Endless_loop:
     loop
       Chute.Hopper_load;
-      delay 0.45;
+      delay 0.55;
       Chute.Hopper_Unload;
-      delay 0.45;
+      delay 0.55;
+      ballsReleased := ballsReleased+1;
+      if ballsReleased > 55 then
+        exit;
+      end if;
     end loop Endless_loop;
   end releaseball;
+  
   task type SortMetalBall;
   task body SortMetalBall is
   begin
-	delay 1.90;
-	Chute.Sorter_Metal;
-	suspend := true;
-	delay 0.45;
-	suspend := false;
+    delay 1.85;
+    Chute.Sorter_Metal;
+    suspend := true;
+    delay 0.55;
+    suspend := false;
   end SortMetalBall;
+  
   type SortMetalBallPointer is access SortMetalBall;
   --type k is array(Positive range <>) of SortMetalBallPointer;
   task type DetectBall;
   task body DetectBall is
     ReleasedBall : Ball_Detected;
-    ReleasedAt   : Time;
-    i			 : Integer;
-	k			 : array(1..70) of SortMetalBallPointer;
+    i : Integer;
+    k : array(1..61) of SortMetalBallPointer;
   begin
-	i := 0;
+    i := 0;
     Endless_loop:
     loop
-	  LastBall := ReleasedBall;
+      LastBall := ReleasedBall;
       Get_Ball(ReleasedBall, ReleasedAt);
       case ReleasedBall is
         when Metal =>
           Text_IO.Put_Line("Metal ball!");
-			if(i = 60) then
-				i := 1;
-			end if;
-			i := i + 1;
-			k(i) := new SortMetalBall;
+          if(i = 60) then
+            i := 1;
+          end if;
+          i := i + 1;
+          k(i) := new SortMetalBall;
         when Unknown =>
-			if suspend = false then
-				Chute.Sorter_Glass;
-			end if;
+          if suspend = false then
+            Chute.Sorter_Glass;
+            Text_IO.Put_Line("Glass Ball!");
+          end if;
         when others =>
           Text_IO.Put_Line("The World Ended. Well Done.");
       end case;
     end loop Endless_loop;
   end DetectBall;
 
-
   WatchBall : DetectBall;
-  r2task:releaseball;
+  r2task    : releaseball;
 
 begin
-	flag := false;
-	suspend := false;
+  suspend := false;
   Text_IO.Put_Line("Starting Cool beans");
 end Main;
